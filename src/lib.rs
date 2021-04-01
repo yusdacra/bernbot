2d176bc40030a27accf48b09c836ebf14034a64e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-01 06:01:27 +0300

    refactor: everything, lol

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,335 @@
-use std::str::SplitWhitespace;
+use std::{
+    collections::HashMap,
+    fmt::{Debug, Display},
+    path::Path,
+    str::SplitWhitespace,
+    sync::Arc,
+};
 
 use markov::Chain;
 use rand::{prelude::IteratorRandom, Rng};
 
-pub const PREFIX: &str = "bern ";
+use parking_lot::Mutex;
+use serde::{Deserialize, Serialize};
 
-pub const POEMS: &str = include_str!("poems.txt");
-pub const INSULTS: &str = include_str!("insults.txt");
-pub const UMAD_IMG: &[u8] = include_bytes!("umad.jpg");
+pub const POEMS: &str = include_str!("../resources/poems.txt");
+pub const INSULTS: &str = include_str!("../resources/insults.txt");
+pub const UMAD_JPG: &[u8] = include_bytes!("../resources/umad.jpg");
+
+type MChain = Chain<String>;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MarkovData {
+    probability: f64,
+    chain: MChain,
+}
+
+impl Default for MarkovData {
+    fn default() -> Self {
+        Self {
+            probability: 5.0,
+            chain: MChain::new(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct InsultData {
+    message_id: Option<String>,
+    count_passed: u8,
+}
+
+impl Default for InsultData {
+    fn default() -> Self {
+        Self {
+            message_id: None,
+            count_passed: 1,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum BotCmd {
+    ReplyWith(String),
+    SendAttachment { name: String, data: Vec<u8> },
+    DoNothing,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct BotData {
+    prefix: String,
+    insult_data: Mutex<HashMap<String, InsultData>>,
+    mchain: Mutex<HashMap<String, MarkovData>>,
+}
+
+#[derive(Debug)]
+pub struct Bot {
+    data: Arc<BotData>,
+    poem_chain: Arc<MChain>,
+}
+
+impl Bot {
+    pub fn new(prefix: impl Display) -> Self {
+        Self {
+            data: Arc::new(BotData {
+                prefix: prefix.to_string(),
+                insult_data: HashMap::new().into(),
+                mchain: HashMap::new().into(),
+            }),
+            poem_chain: {
+                let mut chain = Chain::new();
+                chain.feed_str(&POEMS.replace('-', ""));
+                Arc::new(chain)
+            },
+        }
+    }
+
+    pub fn read_from(data_path: &Path) -> Result<Self, std::io::Error> {
+        let raw = std::fs::read(data_path)?;
+        let data = ron::de::from_bytes(&raw).expect("failed to parse data");
+
+        Ok(Self {
+            data: Arc::new(data),
+            poem_chain: {
+                let mut chain = Chain::new();
+                chain.feed_str(&POEMS.split('-').collect::<String>());
+                Arc::new(chain)
+            },
+        })
+    }
+
+    pub fn save_to(&self, data_path: &Path) -> Result<(), std::io::Error> {
+        std::fs::write(
+            data_path,
+            ron::ser::to_string_pretty(&self.data, ron::ser::PrettyConfig::default())
+                .expect("couldnt serialize"),
+        )
+    }
+
+    pub fn markov_mark_channel(&self, channel_id: &str) -> String {
+        let mut lock = self.data.mchain.lock();
+        if lock.contains_key(channel_id) {
+            "Channel is already marked for listening, you fool.".into()
+        } else {
+            lock.insert(channel_id.to_string(), Default::default());
+            "Will listen to this channel".into()
+        }
+    }
+
+    pub fn markov_unmark_channel(&self, channel_id: &str) -> String {
+        self.data.mchain.lock().remove(channel_id);
+        "Will no longer listen to this channel".into()
+    }
+
+    pub fn markov_set_prob(&self, channel_id: &str, new_prob: &str) -> String {
+        let prob = new_prob.parse().unwrap_or(5.0);
+        if let Some(data) = self.data.mchain.lock().get_mut(channel_id) {
+            data.probability = prob;
+            format!("Set probability to {}%", prob)
+        } else {
+            self.mark_channel_for_listen_msg()
+        }
+    }
+
+    pub fn markov_get_prob(&self, channel_id: &str) -> String {
+        if let Some(data) = self.data.mchain.lock().get(channel_id) {
+            format!("Probability is {}%", data.probability)
+        } else {
+            self.mark_channel_for_listen_msg()
+        }
+    }
+
+    fn mark_channel_for_listen_msg(&self) -> String {
+        format!(
+            "First set a channel to listen in, dumb human.\nA tip: you can do so with `{} listen mark`.",
+            self.data.prefix
+        )
+    }
+
+    pub fn process_listen_command(
+        &self,
+        channel_id: &str,
+        message_id: &str,
+        subcmd: &str,
+        mut args: SplitWhitespace,
+    ) -> String {
+        match subcmd {
+            "mark" => self.markov_mark_channel(channel_id),
+            "unmark" => self.markov_unmark_channel(channel_id),
+            "setprob" => self.markov_set_prob(channel_id, args.next().unwrap_or("5.0")),
+            "getprob" => self.markov_get_prob(channel_id),
+            _ => self.unrecognised_command(channel_id, message_id, subcmd),
+        }
+    }
+
+    pub fn process_args(&self, channel_id: &str, message_id: &str, args: &str) -> BotCmd {
+        let mut args = args.split_whitespace();
+        if let Some("bern") = args.next() {
+            if let Some(cmd) = args.next() {
+                match cmd {
+                    "poem" => BotCmd::ReplyWith(self.process_poem_command(args)),
+                    "fuckyou" => BotCmd::SendAttachment {
+                        name: "umad.jpg".to_string(),
+                        data: UMAD_JPG.to_vec(),
+                    },
+                    "listen" => BotCmd::ReplyWith(if let Some(subcmd) = args.next() {
+                        self.process_listen_command(channel_id, message_id, subcmd, args)
+                    } else {
+                        "commands are:\n- `mark`\n- `unmark`\n- `getprob`\n- `setprob <value>`"
+                            .to_string()
+                    }),
+                    _ => BotCmd::ReplyWith(self.unrecognised_command(channel_id, message_id, cmd)),
+                }
+            } else {
+                BotCmd::ReplyWith("What do you want?".to_string())
+            }
+        } else {
+            BotCmd::DoNothing
+        }
+    }
+
+    pub fn insult(&self, channel_id: &str, message_id: &str) -> String {
+        let mut lock = self.data.insult_data.lock();
+        if !lock.contains_key(channel_id) {
+            lock.insert(channel_id.to_string(), Default::default());
+        }
+        let insult_data = lock.get_mut(channel_id).unwrap();
+        insult_data.count_passed = 1;
+        insult_data.message_id = Some(message_id.to_string());
+        choose_random_insult().to_string()
+    }
+
+    pub fn has_insult_response(
+        &self,
+        channel_id: &str,
+        message_id: &str,
+        message_content: &str,
+    ) -> bool {
+        let lock = self.data.insult_data.lock();
+        if let Some(data) = lock.get(channel_id) {
+            if let Some(msg_id) = data.message_id.as_deref() {
+                if message_content.contains("fuck you") && message_id == msg_id {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    pub fn try_insult(&self, channel_id: &str, message_id: &str) -> Option<String> {
+        let mut lock = self.data.insult_data.lock();
+        if !lock.contains_key(channel_id) {
+            lock.insert(channel_id.to_string(), Default::default());
+        }
+        let insult_data = lock.get_mut(channel_id).unwrap();
+        if rand::thread_rng().gen_bool(0.05 * (insult_data.count_passed as f64) / 100.0) {
+            insult_data.count_passed = 1;
+            insult_data.message_id = Some(message_id.to_string());
+            Some(choose_random_insult().to_string())
+        } else {
+            insult_data.count_passed = insult_data.count_passed.saturating_add(1);
+            None
+        }
+    }
+
+    pub fn markov_try_gen_message(
+        &self,
+        channel_id: &str,
+        message_content: &str,
+    ) -> Option<String> {
+        if let Some(mlisten) = self.data.mchain.lock().get_mut(channel_id) {
+            mlisten.chain.feed_str(message_content);
+            if rand::thread_rng().gen_bool(mlisten.probability / 100.0) {
+                let mut message = mlisten.chain.generate_str();
+                message.truncate(250);
+                return Some(message);
+            }
+        }
+        None
+    }
+
+    pub fn unrecognised_command(&self, channel_id: &str, message_id: &str, cmd: &str) -> String {
+        format!(
+            "{}`{}` isn't even a command.",
+            self.insult(channel_id, message_id),
+            cmd
+        )
+    }
+
+    pub fn process_poem_command(&self, keywords: SplitWhitespace) -> String {
+        let poem_chain = &self.poem_chain;
+        let keywords = keywords.map(str::to_lowercase).collect::<Vec<_>>();
+
+        if keywords.is_empty() {
+            choose_random_poem().to_string()
+        } else if keywords.first().map(|s| s.as_str()) == Some("~gen") {
+            let mut output = String::new();
+            let some_tokens = poem_chain.generate();
+
+            let mut rng = rand::thread_rng();
+            let start_token = some_tokens
+                .iter()
+                .filter(|c| c.chars().next().unwrap().is_uppercase())
+                .choose(&mut rng)
+                .unwrap();
+            let seperate_by = rng.gen_range(2..=3);
+            for (index, sentence) in poem_chain
+                .generate_str_from_token(start_token)
+                .split(|c| matches!(c, '.' | '?' | '\n'))
+                .filter(|sub| !sub.trim().is_empty())
+                .take(7)
+                .enumerate()
+            {
+                let sentence = sentence.trim();
+                output.push_str(sentence);
+                if !sentence.ends_with(',') {
+                    output.push('.');
+                }
+                output.push('\n');
+                if index % seperate_by == 0 {
+                    output.push('\n');
+                }
+            }
+            output
+        } else {
+            let mut ranked = POEMS
+                .split('-')
+                .filter_map(|p| {
+                    let mut has_keywords = 0;
+                    for k in &keywords {
+                        if p.to_lowercase().contains(k) {
+                            has_keywords += 1;
+                        }
+                    }
+                    if has_keywords > 0 {
+                        Some((p, has_keywords))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+            ranked.sort_by_key(|(_, k)| *k);
+            if let Some((result, _)) = ranked.last() {
+                result.to_string()
+            } else {
+                "No poem with those words. Try again, maybe a miracle will occur.".to_string()
+            }
+        }
+    }
+}
 
 pub fn choose_random_poem() -> &'static str {
-    POEMS.split('-').choose(&mut rand::thread_rng()).unwrap()
+    POEMS
+        .split('-')
+        .choose(&mut rand::thread_rng())
+        .expect("always something in poems")
 }
 
 pub fn choose_random_insult() -> &'static str {
-    INSULTS.split('-').choose(&mut rand::thread_rng()).unwrap()
-}
-
-pub fn unrecognised_command(cmd: &str) -> String {
-    format!("{}`{}` isn't even a command.", choose_random_insult(), cmd)
-}
-
-pub fn process_poem_command(keywords: SplitWhitespace, poem_chain: &Chain<String>) -> String {
-    let keywords = keywords.map(str::to_lowercase).collect::<Vec<_>>();
-
-    if keywords.is_empty() {
-        choose_random_poem().to_string()
-    } else if keywords.first().map(|s| s.as_str()) == Some("~gen") {
-        let mut output = String::new();
-        let some_tokens = poem_chain.generate();
-
-        let mut rng = rand::thread_rng();
-        let start_token = some_tokens
-            .iter()
-            .filter(|c| c.chars().next().unwrap().is_uppercase())
-            .choose(&mut rng)
-            .unwrap();
-        let seperate_by = rng.gen_range(2..=3);
-        for (index, sentence) in poem_chain
-            .generate_str_from_token(start_token)
-            .split(|c| matches!(c, '.' | '?' | '\n'))
-            .filter(|sub| !sub.trim().is_empty())
-            .take(7)
-            .enumerate()
-        {
-            let sentence = sentence.trim();
-            output.push_str(sentence);
-            if !sentence.ends_with(',') {
-                output.push('.');
-            }
-            output.push('\n');
-            if index % seperate_by == 0 {
-                output.push('\n');
-            }
-        }
-        output
-    } else {
-        let mut ranked = POEMS
-            .split('-')
-            .filter_map(|p| {
-                let mut has_keywords = 0;
-                for k in &keywords {
-                    if p.to_lowercase().contains(k) {
-                        has_keywords += 1;
-                    }
-                }
-                if has_keywords > 0 {
-                    Some((p, has_keywords))
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>();
-        ranked.sort_by_key(|(_, k)| *k);
-        if let Some((result, _)) = ranked.last() {
-            result.to_string()
-        } else {
-            "No poem with those words. Try again, maybe a miracle will occur.".to_string()
-        }
-    }
+    INSULTS
+        .split('-')
+        .choose(&mut rand::thread_rng())
+        .expect("always something in insults")
 }
 
 #[macro_export]
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use bernbot::perr;
-use markov::Chain;
-use rand::Rng;
-use serde::{Deserialize, Serialize};
+use bernbot::{perr, Bot};
 use serenity::{
     async_trait,
     client::{Client, Context, EventHandler},
-    futures::lock::Mutex,
     http::AttachmentType,
     model::{
         channel::Message,
@@ -13,70 +9,12 @@ use serenity::{
     },
     utils::ContentSafeOptions,
 };
-use std::{borrow::Cow, collections::HashMap, env, num::NonZeroU8};
+use std::{env, path::Path};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct MListen {
-    chan_id: u64,
-    probability: f64,
-    chain: Chain<String>,
-}
-
-impl MListen {
-    async fn save(&self, guild_id: u64) {
-        tokio::fs::write(
-            format!("data/{}", guild_id),
-            bincode::serialize(self).unwrap(),
-        )
-        .await
-        .unwrap();
-    }
-}
-
-impl Default for MListen {
-    fn default() -> Self {
-        Self {
-            chan_id: 0,
-            probability: 5.0,
-            chain: Chain::new(),
-        }
-    }
-}
+const DATA_PATH: &str = "data";
 
 struct Handler {
-    insult_data: Mutex<HashMap<u64, (NonZeroU8, Option<Message>)>>,
-    mchain: Mutex<HashMap<u64, MListen>>,
-    poem_chain: Chain<String>,
-    prefix: &'static str,
-}
-
-impl Default for Handler {
-    fn default() -> Self {
-        Self {
-            insult_data: HashMap::new().into(),
-            mchain: HashMap::new().into(),
-            poem_chain: {
-                let mut chain = Chain::new();
-                chain.feed_str(&bernbot::POEMS.split('-').collect::<String>());
-                chain
-            },
-            prefix: bernbot::PREFIX,
-        }
-    }
-}
-
-impl Handler {
-    async fn unrecognised_command(&self, ctx: &Context, msg: &Message, cmd: &str, channel_id: u64) {
-        perr!(
-            msg.reply(ctx, bernbot::unrecognised_command(cmd)).await,
-            |msg| {
-                self.insult_data
-                    .lock()
-                    .await
-                    .insert(channel_id, (NonZeroU8::new(1).unwrap(), Some(msg)));
-            }
-        )
-    }
+    bot: Bot,
 }
 
 #[async_trait]
@@ -87,148 +25,76 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, new_message: Message) {
-        let guild_id = new_message.guild_id.unwrap().0;
-        let channel_id = new_message.channel_id.0;
-        if let Some(args) = new_message.content.strip_prefix(self.prefix) {
-            let mut args = args.split_whitespace();
-            if let Some(cmd) = args.next() {
-                match cmd {
-                    "poem" => perr!(
-                        new_message
-                            .reply(&ctx, bernbot::process_poem_command(args, &self.poem_chain))
-                            .await
-                    ),
-                    "fuckyou" => perr!(send_fuckyou(&ctx, &new_message).await),
-                    "listen" => {
-                        const SUBCMDS: [&str; 3] = ["here", "setprob", "getprob"];
-
-                        if let Some(subcmd) = args.next() {
-                            if SUBCMDS.contains(&subcmd) {
-                                let mut lock = self.mchain.lock().await;
-                                let msg = match subcmd {
-                                    "here" => {
-                                        let mut mlisten = lock.entry(guild_id).or_default();
-                                        mlisten.chan_id = channel_id;
-                                        mlisten.save(guild_id).await;
-                                        "This channel will be used to post \"random\" messages."
-                                            .to_string()
-                                    }
-                                    "setprob" => {
-                                        let prob = args
-                                            .next()
-                                            .map_or(5.0, |c| c.parse::<f64>().unwrap_or(5.0))
-                                            .min(100.0)
-                                            .max(0.0);
-                                        if let Some(mlisten) = lock.get_mut(&guild_id) {
-                                            mlisten.probability = prob;
-                                            mlisten.save(guild_id).await;
-                                            format!("Set probability to {}%", prob)
-                                        } else {
-                                            "First set a channel to listen in, dumb human."
-                                                .to_string()
-                                        }
-                                    }
-                                    "getprob" => {
-                                        if let Some(mlisten) = lock.get(&guild_id) {
-                                            format!("Probability is {}%", mlisten.probability)
-                                        } else {
-                                            "First set a channel to listen in, dumb human."
-                                                .to_string()
-                                        }
-                                    }
-                                    _ => unreachable!("literally how"),
-                                };
-                                drop(lock);
-                                perr!(new_message.reply(&ctx, msg,).await);
-                            } else {
-                                self.unrecognised_command(&ctx, &new_message, subcmd, channel_id)
-                                    .await
-                            }
-                        }
-                    }
-                    _ => {
-                        self.unrecognised_command(&ctx, &new_message, cmd, channel_id)
-                            .await
-                    }
+        let channel_id = new_message.channel_id.0.to_string();
+        let message_id = new_message.id.to_string();
+        let bot_cmd = self
+            .bot
+            .process_args(&channel_id, &message_id, &new_message.content);
+        if !matches!(bot_cmd, bernbot::BotCmd::DoNothing) {
+            match bot_cmd {
+                bernbot::BotCmd::ReplyWith(content) => {
+                    perr!(new_message.reply(&ctx, content).await)
                 }
+                bernbot::BotCmd::SendAttachment { name, data } => {
+                    perr!(send_attach(&ctx, &new_message, data, name).await)
+                }
+                bernbot::BotCmd::DoNothing => unreachable!(),
             }
         } else {
-            let mut lock = self.insult_data.lock().await;
-
             if let Some(ref_msg) = new_message.referenced_message.as_ref() {
-                if let Some((_, Some(last_ins_msg))) = lock.get(&ref_msg.channel_id.0) {
-                    if new_message.content.contains("fuck you") && ref_msg.id == last_ins_msg.id {
-                        perr!(send_fuckyou(&ctx, &new_message).await);
-                    }
+                if self.bot.has_insult_response(
+                    &channel_id,
+                    &ref_msg.id.to_string(),
+                    &new_message.content,
+                ) {
+                    perr!(
+                        send_attach(
+                            &ctx,
+                            &new_message,
+                            bernbot::UMAD_JPG.to_vec(),
+                            "umad.jpg".to_string()
+                        )
+                        .await
+                    );
                 }
             }
 
             if new_message.author.id != ctx.cache.current_user_id().await {
-                let (no_insult_count, last_ins_msg) = lock
-                    .entry(guild_id)
-                    .or_insert((NonZeroU8::new(1).unwrap(), None));
-                if rand::thread_rng().gen_bool(0.05 * (no_insult_count.get() as f64) / 100.0) {
-                    *no_insult_count = NonZeroU8::new(1).unwrap();
-                    perr!(
-                        new_message
-                            .reply(&ctx, bernbot::choose_random_insult())
-                            .await,
-                        |msg| {
-                            *last_ins_msg = Some(msg);
-                        }
-                    );
-                } else {
-                    *no_insult_count =
-                        NonZeroU8::new(no_insult_count.get().saturating_add(1)).unwrap();
+                if let Some(content) = self.bot.try_insult(&channel_id, &message_id) {
+                    perr!(new_message.reply(&ctx, content).await);
                 }
-                drop(lock);
 
-                if let Some(mlisten) = self.mchain.lock().await.get_mut(&guild_id) {
-                    if mlisten.chan_id == channel_id {
-                        mlisten.chain.feed_str(&new_message.content);
-                        if rand::thread_rng().gen_bool(mlisten.probability / 100.0) {
-                            if let Some(chan) = new_message
-                                .guild(&ctx)
-                                .await
-                                .map(|mut g| g.channels.remove(&mlisten.chan_id.into()))
-                                .flatten()
-                            {
-                                let mut message = mlisten.chain.generate_str();
-                                message.truncate(250);
-                                let message = serenity::utils::content_safe(
-                                    &ctx,
-                                    &message,
-                                    &ContentSafeOptions::default(),
-                                )
-                                .await;
-                                perr!(chan.send_message(&ctx, |msg| msg.content(message)).await);
-                            }
-                        }
-                        mlisten.save(guild_id).await;
+                if let Some(content) = self
+                    .bot
+                    .markov_try_gen_message(&channel_id, &new_message.content)
+                {
+                    let content = serenity::utils::content_safe(
+                        &ctx,
+                        &content,
+                        &ContentSafeOptions::default(),
+                    )
+                    .await;
+                    let cid = new_message.channel_id;
+                    if let Some(chan) = new_message
+                        .guild(&ctx)
+                        .await
+                        .map(|mut g| g.channels.remove(&cid))
+                        .flatten()
+                    {
+                        perr!(chan.send_message(&ctx, |msg| msg.content(content)).await);
                     }
                 }
             }
         }
+        self.bot.save_to(Path::new(DATA_PATH)).unwrap();
     }
 }
 
 fn main() {
     let runtime = tokio::runtime::Runtime::new().unwrap();
 
-    let mut mchain_map = HashMap::new();
-
-    std::fs::create_dir_all("data").unwrap();
-    let dirs = std::fs::read_dir("data").unwrap().flatten();
-    for dir in dirs {
-        let guild_id: u64 = dir.file_name().to_string_lossy().parse().unwrap();
-        let mlisten: MListen = bincode::deserialize(&std::fs::read(dir.path()).unwrap()).unwrap();
-        mchain_map.insert(guild_id, mlisten);
-    }
-
-    let handler = Handler {
-        mchain: mchain_map.into(),
-        ..Handler::default()
-    };
+    let bot = Bot::read_from(Path::new(DATA_PATH)).unwrap_or_else(|_| Bot::new("bern"));
+    let handler = Handler { bot };
 
     let token = env::var("DISCORD_TOKEN").expect("token");
     let mut client = runtime
@@ -248,7 +114,12 @@ fn main() {
     runtime.shutdown_background();
 }
 
-async fn send_fuckyou(ctx: &Context, msg: &Message) -> serenity::Result<()> {
+async fn send_attach(
+    ctx: &Context,
+    msg: &Message,
+    data: Vec<u8>,
+    filename: String,
+) -> serenity::Result<()> {
     let cid = msg.channel_id;
     if let Some(chan) = msg
         .guild(ctx)
@@ -258,8 +129,8 @@ async fn send_fuckyou(ctx: &Context, msg: &Message) -> serenity::Result<()> {
     {
         chan.send_message(ctx, |msg| {
             msg.add_file(AttachmentType::Bytes {
-                data: Cow::Borrowed(bernbot::UMAD_IMG),
-                filename: "umad.jpg".into(),
+                data: data.into(),
+                filename,
             })
         })
         .await?;
