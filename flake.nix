40257b7b45ba57b89c76e19e6b15f641e32ad712
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-15 05:34:32 +0300

    build(nix): switch to nix-cargo-integration

--- flake.nix
+++ flake.nix
@@ -1,57 +1,39 @@
 {
   inputs = {
-    devshell.url = "github:numtide/devshell";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    naersk = {
-      url = "github:nmattia/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flakeUtils.url = "github:numtide/flake-utils";
-    rustOverlay = {
-      url = "github:oxalica/rust-overlay";
+    nixCargoIntegration = {
+      url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: with inputs;
-    with flakeUtils.lib;
+  outputs = inputs:
     let
-      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-    in
-    eachSystem (cargoToml.package.metadata.nix.systems or defaultSystems) (system:
-      let
-        common = import ./nix/common.nix {
-          sources = { inherit devshell naersk nixpkgs rustOverlay; };
-          inherit system cargoToml;
-        };
-
-        lib = common.pkgs.lib;
-        packages = {
-          # Compiles slower but has tests and faster executable
-          "${cargoToml.package.name}" = import ./nix/build.nix {
-            inherit common;
-            doCheck = true;
-            release = true;
+      lib = inputs.nixpkgs.lib;
+      mapToPlatform = platform: lib.mapAttrs (_: lib.mapAttrs' (name: lib.nameValuePair "${name}-${platform}"));
+      mkPlatform = platform:
+        let
+          outputs = inputs.nixCargoIntegration.lib.makeOutputs {
+            root = ./.;
+            overrides = {
+              build = common: prevb: {
+                cargoBuildOptions = def: (prevb.cargoBuildOptions def) ++ [ "--no-default-features" "--features" platform ];
+              };
+            };
           };
-          # Compiles faster but no tests and slower executable
-          "${cargoToml.package.name}-debug" = import ./nix/build.nix { inherit common; };
-        };
-        checks = {
-          # Compiles faster but has tests and slower executable
-          "${cargoToml.package.name}-tests" = import ./nix/build.nix { inherit common; doCheck = true; };
-        };
-        apps = builtins.mapAttrs (n: v: mkApp { name = n; drv = v; exePath = "/bin/${cargoToml.package.metadata.nix.executable or cargoToml.package.name}"; }) packages;
-      in
-      ({
-        devShell = import ./nix/devShell.nix { inherit common; };
-      } // (lib.optionalAttrs (cargoToml.package.metadata.nix.build or false) ({
-        inherit packages checks;
-        # Release build is the default package
-        defaultPackage = packages."${cargoToml.package.name}";
-      } // (lib.optionalAttrs (cargoToml.package.metadata.nix.app or false) {
-        inherit apps;
-        # Release build is the default app
-        defaultApp = apps."${cargoToml.package.name}";
-      }))))
-    );
+        in
+        lib.mapAttrs
+          (name: attrs:
+            if lib.any (x: x == name) [ "apps" "checks" "packages" ]
+            then (mapToPlatform platform) attrs
+            else attrs
+          )
+          outputs;
+      platforms = map mkPlatform [ "discord" "harmony" ];
+      outputs = lib.foldAttrs lib.recursiveUpdate { } platforms;
+    in
+    outputs // {
+      defaultPackage.x86_64-linux = outputs.packages.x86_64-linux.bernbot-discord;
+      defaultApp.x86_64-linux = outputs.apps.x86_64-linux.bernbot-discord;
+    };
 }
