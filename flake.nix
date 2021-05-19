49c7d1d90426ea34cd7a082590dfa5f65b15fa10
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-05-19 04:20:57 +0300

    chore(deps): update flake deps

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixCargoIntegration = {
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
